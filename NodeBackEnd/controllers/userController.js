dc8d5649bad4594856b9d00dd5773f0bde192898
Harry918 <hssachde@uci.edu>
2021-01-16 13:33:43 -0800

    setup basic firebase connection and basic db call

---
+++ NodeBackEnd/controllers/userController.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const firebase = require('../firebase_db');
+const firestore = firebase.firestore();
+const users = firestore.collection('users');
+
+
+/* add new user */
+const addUser = async (name, uid, covid) => {
+  try {
+    const data = {
+      name: name,
+      uid: uid, 
+      covid: covid,
+    };
+    const addedUser = await users.doc(data.uid.toString()).set(data);
+  }
+  catch(error){
+    console.log(error);
+  }
+}
+
+/* update Covid */
+const updateCovid = async (name, uid, covid) => {
+  const data = {
+    name: name,
+    uid: uid, 
+    covid: covid,
+  };
+  const updatedUser = await users.doc(data.uid.toString()).update(data);
+}
+
+module.exports = {
+    addUser, updateCovid
+}
--- NodeBackEnd/index.js
+++ NodeBackEnd/index.js
@@ -1,12 +1,37 @@
+'use strict';
 const http = require('http');
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const config = require('./config');
 
+const { addUser } = require('./controllers/userController');
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
 const server = http.createServer(app);
 
 app.use(cors());
+
+app.get('/', function(req,res) {
+    let covid = true
+    let name = 'BOB'
+    let uid = '22222'
+    addUser(name, uid, covid);
+    console.log('hello');
+    res.send('hello Sid');
+});
+
+app.get('/getName', function(req, res) {
+    res.send('IT WORKed HARRY');
+})
+
+app.post('./getLangLong', function(req, res) {
+    
+})
 
 
 server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
