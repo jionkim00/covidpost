43a494f2613d35ef8847e976e8186e52c9838c9c
Siddartha <sid_sen@berkeley.edu>
2021-01-17 09:10:25 -0800

    changes

--- NodeBackEnd/controllers/userController.js
+++ NodeBackEnd/controllers/userController.js
@@ -1,8 +1,11 @@
 'use strict';
 
+const firebaseAdmin = require('firebase-admin');
+
 const firebase = require('../firebase_db');
 const firestore = firebase.firestore();
 const users = firestore.collection('users');
+const dataTable = firestore.collection('data');
 const tracingpingData = firestore.collection('tracingpingData')
 
 
@@ -15,7 +18,7 @@ const addUser = async (name, uid, covid) => {
       covid: covid,
       contact: []
     };
-    const addedUser = await users.doc(data.uid.toString()).set(data);
+    const addedUser = await users.doc(data.uid.toString()).add(data);
   }
   catch(error){
     console.log(error);
@@ -24,19 +27,15 @@ const addUser = async (name, uid, covid) => {
 
 /* Update Covid. */
 const updateCovidStatus = async (name, uid, covid) => {
-  const data = {
-    name: name,
-    uid: uid, 
-    covid: covid,
-  };
-  const updatedUser = await userTable.doc(data.uid.toString()).update(data);
+  const updatedUser = await users.doc(data.uid).update({covid: covid});
 };
 
-/* Add new user. */
+/* Add lat/long. */
+// whats the point of this
 const addLangLong = async (latitude, longitude, uid) => {
     try {
         //find way to add latitude and longitude
-      const addedUser = await users.doc(data.uid.toString()).set(data);
+      const addedUser = await users.doc(uid).set(data);
     }
     catch(error){
       console.log(error);
@@ -44,28 +43,60 @@ const addLangLong = async (latitude, longitude, uid) => {
   };
 
 /* Remove fields from data table. Automatic after X time. */
-// const cleanDataTable;
+const cleanDataTable = async () => {
+  // filter by date and remove each one
+};
 
 /* Update data table. */
-const updateDataTable = async (name, uid, long, lat, time) => {
+// pass in time or current time?
+const updateDataTable = async (name, uid, lat, long, time) => {
   const data = {
     name: name,
     uid: uid,
-    long: long, 
-    lat: lat,
-    time: time,
+    geopoints: new firebaseAdmin.firestore.GeoPoint(lat, long), 
   };
-  const temp = await dataTable.doc(data.uid.toString()).update(data);
+  const temp = await dataTable.doc(uid.toString()).set(data);
 };
+
+/* Adds UID2 to UID1 close contact. */
+const addCloseContacts = async (uid1, uid2) => {
+  try {
+    
+    const person2 = await users.doc(uid2).get();
+    const person2Data = person2.data();
+
+    // how to get times
+    const data = {
+      name: person2Data.name,
+      uid: person2Data.uid,
+      covid: person2Data.covid,
+    };
+
+    const person1 = await users.doc(uid1).get();
+    person1.data().contact.push(data);
+
+    const contacts = await users.doc(uid1).update({contact: person1.data().contact});
+
+  }
+  catch(error) {
+    console.log(error);
+  }
+}
 
 /* Update close contacts. */
 // const updateCloseContacts;
 
 /* Checks close contact if someone with covid. */
 const checkCloseContacts = async (uid) => {
-  let contacts = await userTable.doc(uid).get();
+  let contacts = await users.doc(uid).get();
+
+  let potentialCovid = contacts.data().contact.filter(function(each) {
+    return each.covid == true;
+  });
+
+  return potentialCovid.length > 0 ? true : false;
 };
 
 module.exports = {
-    addUser, updateCovidStatus, updateDataTable
+    addUser, updateCovidStatus, updateDataTable, checkCloseContacts, addCloseContacts
 }
--- NodeBackEnd/index.js
+++ NodeBackEnd/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const config = require('./config');
 
-const { addUser, updateCovid } = require('./controllers/userController');
+const { addUser, updateCovid, addCloseContacts, updateDataTable } = require('./controllers/userController');
 const { calculateGeolocationData } = require('./geolocation/geolocationConversion')
 
 const app = express();
@@ -45,16 +45,47 @@ app.post('/getLangLong', function(req, res) {
 
 app.put('/updateCovidStatus', function(req, res) {
     try{
-        let name = req. query.name;
+        let name = req.query.name;
         let uid = req.query.uid;
         let covid = req.query.covid;
         updateCovidStatus(name, uid, covid);
         res.send(200).send('Successfully recorded');
     }
     catch(err){
-        res.send(500).send('Server Error')
+        res.send(500).send('Server Error');
     }
 });
+
+// some HTTP issues
+app.get('/addContacts', function(req, res) {
+  try {
+    console.log("index")
+    let uid1 = req.query.uid1;
+    let uid2 = req.query.uid2;
+    addCloseContacts(uid1, uid2);
+    res.sendStatus(200).send('Successfully received');
+  }
+  catch(err) {
+    res.sendStatus(500).send('Server Error');
+  }
+})
+
+// same HTTP issue
+app.put('/updateDataTable', function(req, res) {
+  try {
+    let name = req.query.name;
+    let uid = req.query.uid;
+    let lat = req.query.lat;
+    let long = req.query.long;
+    let time = req.query.time;
+    updateDataTable(name, uid, lat, long, time);
+    res.sendStatus(200).send('Successfully recorded');
+  }
+  catch(err) {
+    res.sendStatus(500).send('Server Error');
+  }
+})
+
 
 app.post('/')
 
